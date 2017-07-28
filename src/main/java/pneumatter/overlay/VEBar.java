abcb39a306869d9824d7eed02723510ef8889a62
Alex <alexmc7777777@gmail.com>
2017-07-28 20:13:39 +0300

    ve bar progress, doesnt yet work but i had no time

--- src/main/java/pneumatter/overlay/VEBar.java
+++ src/main/java/pneumatter/overlay/VEBar.java
@@ -1,15 +1,27 @@
 package pneumatter.overlay;
 
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.lwjgl.opengl.Display;
 import pneumatter.Pneumatter;
 import pneumatter.network.PacketHandler;
 import pneumatter.network.TestPacket;
 
 @Mod.EventBusSubscriber(modid = Pneumatter.MODID)
 public class VEBar extends GuiScreen {
+
+    ResourceLocation text = new ResourceLocation("resources/assets/pneumatter/textures/overlays/vebar.png");
+
+    @Mod.EventHandler
+    public void barRenderer(RenderGameOverlayEvent e){
+        if(e.getType() == RenderGameOverlayEvent.ElementType.CHAT){
+            mc.renderEngine.bindTexture(text);
+            drawTexturedModalRect(Display.getWidth() - 272, Display.getHeight() -128, 0, 0, 256, 256);
+        }
+    }
 
     @SubscribeEvent
     public static void renderOverlayEvent(RenderGameOverlayEvent.Text event) {
